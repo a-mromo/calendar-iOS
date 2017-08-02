2cdbcbd66ebc9410eee77973b23cff5d9d44fe48
agustin Mendoza <agustinoromo@Agustins-MacBook-Pro.local>
2017-08-02 13:48:11 -0700

    Scroll to selected texfField

--- Appt/NewApptTableViewController.swift
+++ Appt/NewApptTableViewController.swift
@@ -12,7 +12,6 @@ import JTAppleCalendar
 
 class NewApptTableViewController: UITableViewController {
   
-  
   var patient: Patient?
   let formatter = DateFormatter()
   
@@ -22,6 +21,7 @@ class NewApptTableViewController: UITableViewController {
   var managedObjectContext: NSManagedObjectContext?
   
   var datePickerHidden = false
+  var calendarViewHidden = false
   
   // Calendar Color
   let outsideMonthColor = UIColor(hexCode: "#584a66")!
@@ -36,22 +36,18 @@ class NewApptTableViewController: UITableViewController {
   
   @IBOutlet weak var patientLabel: UILabel!
   @IBOutlet weak var noteTextView: UITextView!
-
   @IBOutlet weak var costTextField: UITextField!
-  
   @IBOutlet weak var dateDetailLabel: UILabel!
   @IBOutlet weak var datePicker: UIDatePicker!
   
   @IBAction func cancelButton(_ sender: UIBarButtonItem) {
     dismiss(animated: true, completion: nil)
   }
-
+  
   @IBAction func confirmAppointment(_ sender: UIBarButtonItem) {
-    
     let appointment = Appointment(context: persistentContainer.viewContext)
     
     appointment.patient = patient
-//    appointment.date = datePicker.date
     appointment.date = calendarView.selectedDates.first
     appointment.note = noteTextView.text
     appointment.cost = costTextField.text
@@ -69,25 +65,20 @@ class NewApptTableViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    monthLabel.lineBreakMode = .byCharWrapping
     setupCalendarView()
-    datePickerChanged()
     noLargeTitles()
     setTextFieldDelegates()
     setTextViewDelegates()
     setDoneOnKeyboard()
     noteTextView.placeholder = "Notes"
+
+    setupKeyboardNotification()
     
   }
   
-  func setupCalendarView() {
-    // Setup Calendar Spacing
-    calendarView.minimumLineSpacing = 0
-    calendarView.minimumInteritemSpacing = 0
-    
-    // Setup Labels
-    calendarView.visibleDates{ (visibleDates) in
-      self.setupViewsFromCalendar(from: visibleDates)
-    }
+  deinit {
+    NotificationCenter.default.removeObserver(self)
   }
   
   override func viewWillAppear(_ animated: Bool) {
@@ -96,20 +87,53 @@ class NewApptTableViewController: UITableViewController {
     }
   }
   
-  
   func noLargeTitles(){
     if #available(iOS 11.0, *) {
       navigationItem.largeTitleDisplayMode = .never
       tableView.dragDelegate = self as? UITableViewDragDelegate
     }
   }
+
+  func setupKeyboardNotification() {
+    NotificationCenter.default.addObserver(self, selector: #selector(NewApptTableViewController.keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(NewApptTableViewController.keyboardWillHide(notification:)), name: NSNotification.Name.UIKeyboardDidHide, object: nil)
+  }
   
+  @objc func keyboardWillShow(notification: NSNotification) {
+    if let keyboardHeight = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.height {
+      tableView.contentInset = UIEdgeInsetsMake(0, 0, keyboardHeight + 20, 0)
+    }
+  }
+  
+  @objc func keyboardWillHide(notification: NSNotification) {
+    UIView.animate(withDuration: 0.2, animations: {
+      // For some reason adding inset in keyboardWillShow is animated by itself but removing is not, that's why we have to use animateWithDuration here
+      self.tableView.contentInset = UIEdgeInsetsMake(0, 0, 0, 0)
+    })
+  }
+  
+
+  
+  override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+    if section == 0 {
+      return 1
+    } else {
+      return 22
+    }
+  }
+}
+
+
+
+// Date picker
+extension NewApptTableViewController {
   @IBAction func datePickerValue(_ sender: UIDatePicker) {
     datePickerChanged()
   }
   
+  // Run datePickerChanged() in viewDidLoad() to display selected date in Label
   func datePickerChanged() {
-    dateDetailLabel.text = DateFormatter.localizedString(from: datePicker.date, dateStyle: DateFormatter.Style.short, timeStyle: DateFormatter.Style.short)
+//    dateDetailLabel.text = DateFormatter.localizedString(from: datePicker.date, dateStyle: DateFormatter.Style.short, timeStyle: DateFormatter.Style.short)
   }
   
   func toggleDatePicker() {
@@ -119,24 +143,24 @@ class NewApptTableViewController: UITableViewController {
     tableView.endUpdates()
   }
   
+  // Toggle DatePicker
   
-  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    if indexPath.section == 0 && indexPath.row == 0 {
-      toggleDatePicker()
-    }
-    if indexPath.section == 1 && indexPath.row == 0 {
-    }
-  }
-  
-  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-    if datePickerHidden && indexPath.section == 0 && indexPath.row == 1 {
-      return 0
-    } else {
-      return super.tableView(tableView, heightForRowAt: indexPath)
-    }
-  }
-  
+//  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//    if indexPath.section == 0 && indexPath.row == 0 {
+//      toggleDatePicker()
+//    }
+//  }
+//
+//  override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//    // Use the datePicker row. The one below is from a previous iteration
+//    if datePickerHidden && indexPath.section == 0 && indexPath.row == 1 {
+//      return 0
+//    } else {
+//      return super.tableView(tableView, heightForRowAt: indexPath)
+//    }
+//  }
 }
+
 
 extension NewApptTableViewController {
   func handleCellSelected(view: JTAppleCell?, cellState: CellState) {
@@ -161,10 +185,19 @@ extension NewApptTableViewController {
       } else {
         validCell.dateLabel.textColor = outsideMonthColor
       }
-      
     }
   }
   
+  func setupCalendarView() {
+    // Setup Calendar Spacing
+    calendarView.minimumLineSpacing = 0
+    calendarView.minimumInteritemSpacing = 0
+    
+    // Setup Labels
+    calendarView.visibleDates{ (visibleDates) in
+      self.setupViewsFromCalendar(from: visibleDates)
+    }
+  }
   
   func setupViewsFromCalendar(from visibleDates: DateSegmentInfo ) {
     guard let date = visibleDates.monthDates.first?.date else { return }
@@ -175,6 +208,11 @@ extension NewApptTableViewController {
     self.formatter.dateFormat = "MMMM"
     self.monthLabel.text = self.formatter.string(from: date)
   }
+  
+//  func calendarViewDateChanged() {
+//    guard let calendarDate = calendarView.selectedDates.first else { return }
+//    dateDetailLabel.text = DateFormatter.localizedString(from: calendarDate, dateStyle: DateFormatter.Style.short, timeStyle: DateFormatter.Style.short)
+//  }
 }
 
 
@@ -183,7 +221,7 @@ extension NewApptTableViewController: JTAppleCalendarViewDataSource {
     formatter.dateFormat = "yyyy MM dd"
     formatter.timeZone = Calendar.current.timeZone
     formatter.locale = Calendar.current.locale
-   
+    
     var parameters: ConfigurationParameters
     var startDate = Date()
     var endDate = Date()
@@ -214,7 +252,7 @@ extension NewApptTableViewController: JTAppleCalendarViewDelegate {
   func calendar(_ calendar: JTAppleCalendarView, didSelectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
     handleCellSelected(view: cell, cellState: cellState)
     handleCellTextColor(view: cell, cellState: cellState)
-    
+//    calendarViewDateChanged()
   }
   
   func calendar(_ calendar: JTAppleCalendarView, didDeselectDate date: Date, cell: JTAppleCell?, cellState: CellState) {
@@ -227,11 +265,6 @@ extension NewApptTableViewController: JTAppleCalendarViewDelegate {
   }
   
 }
-
-
-
-
-
 
 
 
