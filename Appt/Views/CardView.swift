ed823b0b289d0174c21722b37844947239fa3259
agustin Mendoza <agustinoromo@agustins-macbook-pro.local>
2017-10-09 11:47:17 -0700

    Fixed rounded corners for CardView class

--- Appt/Views/CardView.swift
+++ Appt/Views/CardView.swift
@@ -10,16 +10,25 @@ import UIKit
 
 class CardView: UIView {
   
+  
+  
   override func draw(_ rect: CGRect) {
     
+    self.layer.cornerRadius = 8.0
     self.layer.shadowColor = UIColor.black.cgColor
     self.layer.shadowOffset = CGSize(width: 0, height: 4)
     self.layer.shadowOpacity = 0.2
     self.layer.shadowRadius = 4
     self.clipsToBounds = false
     self.layer.masksToBounds = false
-    self.layer.cornerRadius = 8.0
     
+    let card = UIView()
+    card.frame = self.bounds
+    card.layer.cornerRadius = 8
+    card.backgroundColor = .white
+    card.layer.masksToBounds = true
+    self.addSubview(card)
+    self.sendSubview(toBack: card)
   }
 }
 
@@ -34,6 +43,14 @@ class CardViewLight: UIView {
     self.clipsToBounds = false
     self.layer.masksToBounds = false
     self.layer.cornerRadius = 8.0
+    
+    let card = UIView()
+    card.frame = self.bounds
+    card.layer.cornerRadius = 8
+    card.backgroundColor = .white
+    card.layer.masksToBounds = true
+    self.addSubview(card)
+    self.sendSubview(toBack: card)
     
   }
 }
